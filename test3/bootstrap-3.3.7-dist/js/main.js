626d2c3d08a5fe212d16431888259f383a72dd66
380508025999 <mashenka_angeloshek@mail.ru>
2017-05-08 18:32:46 +0300

    fgf

--- test3/bootstrap-3.3.7-dist/js/main.js
+++ test3/bootstrap-3.3.7-dist/js/main.js
@@ -7,10 +7,9 @@
         $('.section-slider_slider').slick(
             {
                 // arrows:true, //отключить стрелочки
-                autoplay:true, //автоматически перелистівается
-                autoplaySpeed:2000,//перелистівается с интервалом 1 с
-                dots:true,//внизу точечки
-
+                autoplay: true, //автоматически перелистівается
+                autoplaySpeed: 2000,//перелистівается с интервалом 1 с
+                dots: true,//внизу точечки
 
 
             }
@@ -22,18 +21,56 @@
     $(function () {
 
 
-
         $('.section-slider_slider-two').slick(
             {
                 dots: false,
-                autoplay:true,
-                autoplaySpeed:10000,
+                autoplay: true,
+                autoplaySpeed: 10000,
                 slidesToShow: 3,
                 slidesToScroll: 1
             }
-
         )
 
     });
 
+
+        window.onload = function () {
+            var map,
+                point = {lat:-7.926233, lng: 112.632564}
+
+
+            function initMap() {
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: point,
+                    zoom: 16,
+                    disableDefaultUI: true //чтобы наши элемены управления исчезли
+                });
+                var image = {
+                    url: 'Pin.png',
+                    // size: new google.maps.Size(20,32)
+                }
+                var marker = new google.maps.Marker({
+                    position: point,
+                    map: map,
+                    icon: image
+                });
+
+            }
+
+            initMap();
+        }
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.form-scroll').fadeIn();
+        } else {
+            $('.form-scroll').fadeOut();
+        }
+    });
+    $('.form-scroll').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000);
+        return false;
+    });
 })(jQuery);
--- test3/bootstrap-3.3.7-dist/js/main2.js
+++ test3/bootstrap-3.3.7-dist/js/main2.js
@@ -1,21 +1,17 @@
 /**
- * Created by Елена on 07.05.2017.
+ * Created by Елена on 08.05.2017.
  */
-;(function ($) {
 
-
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 0) {
-                $('.learn-more').fadeIn();
-            } else {
-                $('.learn-more').fadeOut();
-            }
-        });
-    $('.learn-more').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 2000);
-        return false;
-    });
-
-})(jQuery);
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 0) {
+//         $('.form-scroll').fadeIn();
+//     } else {
+//         $('.form-scroll').fadeOut();
+//     }
+// });
+// $('.form-scroll').click(function () {
+//     $('body,html').animate({
+//         scrollTop: 0
+//     }, 400);
+//     return false;
+// });
