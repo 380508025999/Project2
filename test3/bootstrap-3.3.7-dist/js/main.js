a6f662fc8567e6d67ea1cc1e026a31eea79badfb
380508025999 <mashenka_angeloshek@mail.ru>
2017-05-09 17:52:51 +0300

    dd

--- test3/bootstrap-3.3.7-dist/js/main.js
+++ test3/bootstrap-3.3.7-dist/js/main.js
@@ -52,31 +52,31 @@
     });
 
 
-        window.onload = function () {
-            var map,
-                point = {lat:-7.926233, lng: 112.632564}
+    window.onload = function () {
+        var map,
+            point = {lat: -7.926233, lng: 112.632564}
 
 
-            function initMap() {
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: point,
-                    zoom: 16,
-                    disableDefaultUI: true //чтобы наши элемены управления исчезли
-                });
-                var image = {
-                    url: 'Pin.png',
-                    // size: new google.maps.Size(20,32)
-                }
-                var marker = new google.maps.Marker({
-                    position: point,
-                    map: map,
-                    icon: image
-                });
-
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: point,
+                zoom: 16,
+                disableDefaultUI: true //чтобы наши элемены управления исчезли
+            });
+            var image = {
+                url: 'Pin.png',
+                // size: new google.maps.Size(20,32)
             }
+            var marker = new google.maps.Marker({
+                position: point,
+                map: map,
+                icon: image
+            });
 
-            initMap();
         }
+
+        initMap();
+    }
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
@@ -91,8 +91,8 @@
         }, 2000);
         return false;
     });
-    $(document).ready(function(){
-        $('.about').click( function(){ // ловим клик по ссылке с классом go_to
+    $(document).ready(function () {
+        $('.about').click(function () { // ловим клик по ссылке с классом go_to
             console.log('start');
             //var scroll_el = $(this).attr('.main-content'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
             var scroll_el = $('.main-content'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
@@ -102,15 +102,15 @@
 
             if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
                 console.log('start 2');
-                $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
-            }else{
+                $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 2000); // анимируем скроолинг к элементу scroll_el
+            } else {
                 console.log('start 3');
             }
             return false; // выключаем стандартное действие
         });
     });
-    $(document).ready(function(){
-        $('.serv').click( function(){ // ловим клик по ссылке с классом go_to
+    $(document).ready(function () {
+        $('.serv').click(function () { // ловим клик по ссылке с классом go_to
             console.log('start');
             //var scroll_el = $(this).attr('.main-content'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
             var scroll_el = $('.main-grid'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
@@ -120,15 +120,15 @@
 
             if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
                 console.log('start 2');
-                $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
-            }else{
+                $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 2000); // анимируем скроолинг к элементу scroll_el
+            } else {
                 console.log('start 3');
             }
             return false; // выключаем стандартное действие
         });
     });
-    $(document).ready(function(){
-        $('.work').click( function(){ // ловим клик по ссылке с классом go_to
+    $(document).ready(function () {
+        $('.work').click(function () { // ловим клик по ссылке с классом go_to
             console.log('start');
             //var scroll_el = $(this).attr('.main-content'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
             var scroll_el = $('.section-slider'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
@@ -138,15 +138,15 @@
 
             if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
                 console.log('start 2');
-                $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
-            }else{
+                $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 2000); // анимируем скроолинг к элементу scroll_el
+            } else {
                 console.log('start 3');
             }
             return false; // выключаем стандартное действие
         });
     });
-    $(document).ready(function(){
-        $('.contact').click( function(){ // ловим клик по ссылке с классом go_to
+    $(document).ready(function () {
+        $('.contact').click(function () { // ловим клик по ссылке с классом go_to
             console.log('start');
             //var scroll_el = $(this).attr('.main-content'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
             var scroll_el = $('.section-slider_two'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
@@ -156,22 +156,22 @@
 
             if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
                 console.log('start 2');
-                $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
-            }else{
+                $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 2000); // анимируем скроолинг к элементу scroll_el
+            } else {
                 console.log('start 3');
             }
             return false; // выключаем стандартное действие
         });
     });
 
-    $(document).ready(function(){
-        $('#fly-block1_icon').click(function(){
+    $(document).ready(function () {
+        $('#fly-block1_icon').click(function () {
             $(this).parent().children('div#fly-block1_text').toggle('normal');
             return false;
         });
     });
-    $(document).ready(function(){
-        $('#fly-block2_icon').click(function(){
+    $(document).ready(function () {
+        $('#fly-block2_icon').click(function () {
             $(this).parent().children('div#fly-block2_text').toggle('normal');
             return false;
         });
