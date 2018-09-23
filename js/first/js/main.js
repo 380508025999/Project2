3b16630a90918a06366fc556bb01a74e7572288c
380508025999 <mashenka_angeloshek@mail.ru>
2018-09-23 20:50:45 +0400

    firstJs

---
+++ js/first/js/main.js
@@ -0,0 +1,8 @@
+let userName = prompt ( 'What is your name ?',' '),
+    userAge = prompt('How old are you ?',' ');
+
+alert ('Thank you. Welcome!');
+
+console.log( userName);
+console.log(userAge);
+
